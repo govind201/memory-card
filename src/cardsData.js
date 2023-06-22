95ebd2b3a89892409ad718f672aced927a84ebd8
Govind Singh <gs5818475@gmail.com>
2023-06-22 23:34:19 +0530

    separe cards data

---
+++ src/cardsData.js
@@ -0,0 +1,16 @@
+const cardsData = [
+  { id: 1, image: "https://www.thiswaifudoesnotexist.net/example-1.jpg" },
+  { id: 2, image: "https://www.thiswaifudoesnotexist.net/example-2.jpg" },
+  { id: 3, image: "https://www.thiswaifudoesnotexist.net/example-11.jpg" },
+  { id: 4, image: "https://www.thiswaifudoesnotexist.net/example-40.jpg" },
+  { id: 5, image: "https://www.thiswaifudoesnotexist.net/example-50.jpg" },
+  { id: 6, image: "https://www.thiswaifudoesnotexist.net/example-60.jpg" },
+  { id: 7, image: "https://www.thiswaifudoesnotexist.net/example-70.jpg" },
+  { id: 8, image: "https://www.thiswaifudoesnotexist.net/example-80.jpg" },
+  { id: 9, image: "https://www.thiswaifudoesnotexist.net/example-90.jpg" },
+  { id: 10, image: "https://www.thiswaifudoesnotexist.net/example-100.jpg" },
+  { id: 11, image: "https://www.thiswaifudoesnotexist.net/example-110.jpg" },
+  { id: 12, image: "https://www.thiswaifudoesnotexist.net/example-120.jpg" },
+];
+
+export default cardsData;
